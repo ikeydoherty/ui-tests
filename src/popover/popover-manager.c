8a769336b625296d0fe69f5dd6f4d2b87703c44e
Ikey Doherty <ikey@solus-project.com>
2017-06-21 21:42:30 +0100

    Add skeleton for new BudgiePopoverManager
    Signed-off-by: Ikey Doherty <ikey@solus-project.com>

---
+++ src/popover/popover-manager.c
@@ -0,0 +1,84 @@
+/*
+ * This file is part of ui-tests
+ *
+ * Copyright © 2016-2017 Ikey Doherty <ikey@solus-project.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ */
+
+#define _GNU_SOURCE
+
+#include "util.h"
+
+BUDGIE_BEGIN_PEDANTIC
+#include "popover-manager.h"
+#include <gtk/gtk.h>
+BUDGIE_END_PEDANTIC
+
+struct _BudgiePopoverManagerClass {
+        GObjectClass parent_class;
+};
+
+struct _BudgiePopoverManager {
+        GObject parent;
+};
+
+G_DEFINE_TYPE(BudgiePopoverManager, budgie_popover_manager, G_TYPE_OBJECT)
+
+/**
+ * budgie_popover_manager_new:
+ *
+ * Construct a new BudgiePopoverManager object
+ */
+BudgiePopoverManager *budgie_popover_manager_new()
+{
+        return g_object_new(BUDGIE_TYPE_POPOVER_MANAGER, NULL);
+}
+
+/**
+ * budgie_popover_manager_dispose:
+ *
+ * Clean up a BudgiePopoverManager instance
+ */
+static void budgie_popover_manager_dispose(GObject *obj)
+{
+        G_OBJECT_CLASS(budgie_popover_manager_parent_class)->dispose(obj);
+}
+
+/**
+ * budgie_popover_manager_class_init:
+ *
+ * Handle class initialisation
+ */
+static void budgie_popover_manager_class_init(BudgiePopoverManagerClass *klazz)
+{
+        GObjectClass *obj_class = G_OBJECT_CLASS(klazz);
+
+        /* gobject vtable hookup */
+        obj_class->dispose = budgie_popover_manager_dispose;
+}
+
+/**
+ * budgie_popover_manager_init:
+ *
+ * Handle construction of the BudgiePopoverManager
+ */
+static void budgie_popover_manager_init(__budgie_unused__ BudgiePopoverManager *self)
+{
+}
+
+/*
+ * Editor modelines  -  https://www.wireshark.org/tools/modelines.html
+ *
+ * Local variables:
+ * c-basic-offset: 8
+ * tab-width: 8
+ * indent-tabs-mode: nil
+ * End:
+ *
+ * vi: set shiftwidth=8 tabstop=8 expandtab:
+ * :indentSize=8:tabSize=8:noTabs=true:
+ */
---
+++ src/popover/popover-manager.h
@@ -0,0 +1,50 @@
+/*
+ * This file is part of ui-tests
+ *
+ * Copyright © 2016-2017 Ikey Doherty <ikey@solus-project.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ */
+
+#pragma once
+
+#include <glib-object.h>
+#include <gtk/gtk.h>
+
+G_BEGIN_DECLS
+
+typedef struct _BudgiePopoverManager BudgiePopoverManager;
+typedef struct _BudgiePopoverManagerClass BudgiePopoverManagerClass;
+
+#define BUDGIE_TYPE_POPOVER_MANAGER budgie_popover_manager_get_type()
+#define BUDGIE_POPOVER_MANAGER(o)                                                                  \
+        (G_TYPE_CHECK_INSTANCE_CAST((o), BUDGIE_TYPE_POPOVER_MANAGER, BudgiePopoverManager))
+#define BUDGIE_IS_POPOVER_MANAGER(o) (G_TYPE_CHECK_INSTANCE_TYPE((o), BUDGIE_TYPE_POPOVER_MANAGER))
+#define BUDGIE_POPOVER_MANAGER_CLASS(o)                                                            \
+        (G_TYPE_CHECK_CLASS_CAST((o), BUDGIE_TYPE_POPOVER_MANAGER, BudgiePopoverManagerClass))
+#define BUDGIE_IS_POPOVER_MANAGER_CLASS(o)                                                         \
+        (G_TYPE_CHECK_CLASS_TYPE((o), BUDGIE_TYPE_POPOVER_MANAGER))
+#define BUDGIE_POPOVER_MANAGER_GET_CLASS(o)                                                        \
+        (G_TYPE_INSTANCE_GET_CLASS((o), BUDGIE_TYPE_POPOVER_MANAGER, BudgiePopoverManagerClass))
+
+BudgiePopoverManager *budgie_popover_manager_new(void);
+
+GType budgie_popover_manager_get_type(void);
+
+G_END_DECLS
+
+/*
+ * Editor modelines  -  https://www.wireshark.org/tools/modelines.html
+ *
+ * Local variables:
+ * c-basic-offset: 8
+ * tab-width: 8
+ * indent-tabs-mode: nil
+ * End:
+ *
+ * vi: set shiftwidth=8 tabstop=8 expandtab:
+ * :indentSize=8:tabSize=8:noTabs=true:
+ */
